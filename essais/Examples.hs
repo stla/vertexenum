324501fd63ddc757103206a3663a94fe59356c2b
stla <laurent_step@yahoo.fr>
2024-05-07 13:39:47 +0200

    examples

--- essais/Examples.hs
+++ essais/Examples.hs
@@ -4,13 +4,13 @@ import Data.Ratio           ( (%) )
 import Data.VectorSpace     ( AdditiveGroup((^-^)), VectorSpace((*^)) )
 import Geometry.VertexEnum
 
-testSmall :: [Constraint]
+testSmall :: [Constraint Double]
 testSmall = [ x .<= 1, x .>= 0, y .<= 1]
   where
     x = newVar 1
     y = newVar 2
 
-rggConstraints :: [Constraint]
+rggConstraints :: [Constraint Double]
 rggConstraints =
   [ x .>= (-5)
   , x .<=  4
@@ -23,20 +23,20 @@ rggConstraints =
     y = newVar 2
     z = newVar 3
 
-region3D :: [Constraint]
-region3D =
-  [ x .>=  0 -- shortcut for x .>=. cst 0
-  , x .<=  3
-  , y .>=  0
-  , y .<=. cst 2 ^-^ (2%3)*^x
-  , z .>=  0
-  , z .<=. cst 6 ^-^ 2*^x ^-^ 3*^y ]
-  where
-    x = newVar 1
-    y = newVar 2
-    z = newVar 3
+-- region3D :: [Constraint Double]
+-- region3D =
+--   [ x .>=  0 -- shortcut for x .>=. cst 0
+--   , x .<=  3
+--   , y .>=  0
+--   , y .<=. cst 2 ^-^ (2%3)*^x
+--   , z .>=  0
+--   , z .<=. cst 6 ^-^ 2*^x ^-^ 3*^y ]
+--   where
+--     x = newVar 1
+--     y = newVar 2
+--     z = newVar 3
 
-cubeConstraints :: [Constraint]
+cubeConstraints :: [Constraint Double]
 cubeConstraints =
   [ x .<= 1
   , x .>= (-1)
